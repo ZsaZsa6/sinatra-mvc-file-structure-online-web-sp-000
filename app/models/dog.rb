d03b30295254c54e166cc295bed934fe3260d2a1
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-20 22:15:01 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,13 +2,12 @@ class Dog
   @@all = []
   attr_accessor :name, :breed, :age
   def initialize(name, breed, age)
-
     @name = name
     @breed = breed
     @age =age
   end
   def self.save
-   @@all<< self
+   @@all << self
 
   end
 
